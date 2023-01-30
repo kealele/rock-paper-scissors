4ec47166a9fe02f0f3ea7853b31d3787eec6d426
kealele <whiski20@gmail.com>
2023-01-30 18:20:21 +0200

    Completed one play round section

--- javascript.js
+++ javascript.js
@@ -1,10 +1,46 @@
 const playerSelection = "rock";
 let weapons = ["rock", "paper", "scissors"];
 const computerSelection = getComputerChoise();
+
 function getComputerChoise() {
     let random = Math.floor(Math.random() * weapons.length);
     return weapons[random];
 }
 
-console.log(getComputerChoise(weapons))
+getComputerChoise();
 
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper"){
+            return("You Lose! Paper beats Rock.");
+        }
+        else if (computerSelection == "scissors"){
+            return("You win! Rock beats scissors.");
+        }
+        else {
+           return("Draw.");
+        }
+    }
+    if (playerSelection == "paper") {
+        if (computerSelection == "scissors"){
+            return("You Lose! Scissors beats Paper.");
+        }
+        else if (computerSelection == "rock"){  
+            return("You win! Paper beats rock.");
+        }
+        else {
+           return("Draw.");
+        }
+    } 
+    if (playerSelection == "scissors") {
+        if (computerSelection == "rock"){
+            return("You Lose! Rock beats Scissors.");
+        }
+        else if (computerSelection == "paper"){
+            return("You win! Scissors beats Paper.");
+        }
+        else {
+           return("Draw.");
+        }
+    }  
+}
